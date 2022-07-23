81995ae05c7de7844a329d25aa63cfc78f7e524a
saribroudy <saripaigebroudy@gmail.com>
2022-07-23 16:25:38 -0400

    queries

---
+++ Queries/queries.sql
@@ -0,0 +1,12 @@
+-- Database: PH-EmployeeDB
+
+-- DROP DATABASE IF EXISTS "PH-EmployeeDB";
+
+CREATE DATABASE "PH-EmployeeDB"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
